a3f18332a4924b859117115b6bf7195ce1b9c867
CapiAlex <alexandersotobarco@gmail.com>
2023-05-13 09:38:13 -0500

    Recorrer arreglos

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,6 +6,22 @@ export function Home() {
             <section className="banner text-white fw-bold">
                 <h2>THE BARBER SHOP...</h2>
             </section>
+            <section>
+                <div className="container my-5">
+                    <div className="row">
+                        <div className="col-12 col-md-8">
+                            <h2>TITULO</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, minus dolorem? Eveniet in eum totam culpa laborum commodi. Libero voluptatum soluta vero et, quis quisquam recusandae facilis voluptatibus beatae perferendis.</p>
+                        </div>
+                        <div className="col-12 col-md-4">
+                            <img src="../../src/assets/img/corte.avif" alt="corte" 
+                            className="img-fluid w-100"/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="banner2">
+            </section>
         </>
     )
 }
--- src/Services/Services.jsx
+++ src/Services/Services.jsx
@@ -1,20 +1,51 @@
 export function Services() {
 
+    let serviciosbarberia = [
+        {
+            nombre: "Afeitada profesional",
+            precio: 20000,
+            foto: "../../src/assets/img/afeitada.jpg",
+            descripcion: "Afeitada con estilos vikingos, el valhala sera tu experiencia"
+        },
+        {
+            nombre: "Corte de pelo",
+            precio: 40000,
+            foto: "../../src/assets/img/corte.avif",
+            descripcion: "Se corta tu pelo con elegancia"
+        },
+        {
+            nombre: "Cuidado de la piel",
+            precio: 160000,
+            foto: "../../src/assets/img/mascarilla.jpg",
+            descripcion: "Mascarillas y lechugas para tus ojos"
+        }
+    ]
+
     return (
         <>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h3>WELCOME</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam praesentium earum ducimus placeat facilis autem mollitia molestiae magni unde reprehenderit nulla harum quasi deserunt velit quae eaque, maxime voluptate corporis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum alias quos esse vero culpa quidem molestiae, accusamus nisi eius natus inventore earum magnam praesentium aut voluptatem expedita distinctio labore cumque!</p>
-                    </div>
-                    <div className="col-12">
-                        <h2>Modern Cuthairs</h2>
-                        <h2>Professional Shave</h2>
-                        <h2>Skin Care</h2>
-                    </div>
+        <div className="container mt-5">
+            <div className="row row-cols-1 row-cols-md-3">
+                {
+                    serviciosbarberia.map(function (service) { 
+                        return(
+                            <div className="col mt-3">
+                                <div className="card h-100 shadow text-center">
+                                    <h1>{service.nombre}</h1>
+                                    <img src={service.foto} className="img-fluid w-100" />
+                                    <p>{service.descripcion}</p>
+                                    <h3>Desde COP($) {service.precio} </h3>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <h1>Por favor estudien</h1>
                 </div>
             </div>
+        </div>
         </>
     )
 }
